a40b9446976961305d6a385bb5b9d5fc0523693d
ashaforddesigns <ashaforddesigns@gmail.com>
2024-12-02 19:43:46 -0600

    kind of works

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 from yt_dlp import YoutubeDL
 from tkinter import *
 from tkinter import ttk
+from tkinter import filedialog
+import os
 
+ytdl = YoutubeDL()
 
+#get download folder regardlesss of os, os.path.join appends the user path with "Downloads" therefore giving me their downloads folder
+downloadsLocation = os.path.join(os.path.expanduser("~"), "Downloads")
 
+ 
 def download_def(url):
-    ytdl = YoutubeDL()
-    info = ytdl(url)
-    print(info["title"])
-    
+    getURL = url.get()
+    ytdl.download([getURL])
+
+
+
+def browse():
+    file_path = filedialog.asksaveasfilename()
+
 
 
 root = Tk()
@@ -21,5 +31,10 @@ ttk.Label(mainFrame, text="Enter your YouTube video below").grid(row=0, column=1
 
 url = StringVar()
 ttk.Entry(mainFrame, takefocus=True, textvariable=url).grid(row=1, column=1, columnspan=2)
-ttk.Button(mainFrame, text="download", command=lambda: download_def(url)).grid(row=1, column=3)
+ttk.Button(mainFrame, text="download", command=lambda: download_def(url)).grid(row=1, column=3, sticky=(W))
+
+ttk.Label(mainFrame, text="Your video will automatically download to your downloads folder unless you hit the 'Save as...' below").grid(row=2, column=1, columnspan=4)
+ttk.Button(mainFrame, text="Save as...", command=lambda: browse()).grid(row=3, column=4)
+
+
 root.mainloop() # you need this line for the gui to actually run
